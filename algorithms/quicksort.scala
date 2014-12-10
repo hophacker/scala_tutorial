71c9b3de83eda63aa5d42a2f02e2d46c38f35792
Jie Feng <jiefeng.hopkins@gmail.com>
2014-12-10 15:02:35 -0500

    algorithm

---
+++ algorithms/quicksort.scala
@@ -0,0 +1,15 @@
+def sort(xs: Array[Int]): Array[Int] = {
+  if (xs.length <= 1) xs
+  else{
+    val pivot = xs(xs.length/2)
+    Array.concat(
+      //def filter(p: T => Boolean): Array[T]
+      sort(xs filter (pivot >)),
+      xs filter (pivot ==),
+      sort(xs filter (pivot <))
+      )
+  }
+}
+var arr = Array(1, 3, 2)
+arr = sort(arr)
+for (i <- 0 to (arr.length-1)) print(arr(i))
